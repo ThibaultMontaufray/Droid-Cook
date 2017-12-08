ec050e6e6094838d2cc6b96915c897ce35ac16f3
Thibault MONTAUFRAY <amostrack@gmail.com>
2017-12-08 15:32:20 +1300

    remove tests comments

---
+++ Project/Model/Intolerance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Droid_Cook.Model
+{
+    public enum Intolerance
+    {
+        DAIRY,
+        EGG,
+        GLUTEN,
+        GRAINS,
+        PEANUT,
+        SEAFOOD,
+        SESAME,
+        SHELLFISH,
+        SOY,
+        TREENUT,
+        WHEAT
+    }
+}
